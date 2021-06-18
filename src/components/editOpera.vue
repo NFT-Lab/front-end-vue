8741cc3394a42cd0f3b1cb78b5219c8ce86922f0
Margherita <margherita.mitillo@gmail.com>
2021-06-18 10:32:59 +0200

    Fix bug edit opera

--- src/components/editOpera.vue
+++ src/components/editOpera.vue
@@ -1,28 +1,16 @@
 <template>
   <v-dialog v-model="dialog" width="60%">
     <template v-slot:activator="{ on, attrs }">
-      <v-icon
-        v-bind="attrs"
-        v-on="on"
-      >
+      <v-icon v-bind="attrs" v-on="on">
         edit
       </v-icon>
     </template>
     <v-card class="pa-3">
       <v-flex>
         <v-form>
-          <v-text-field
-            v-model="visiblePage.title"
-            label="Titolo"
-          />
-          <v-text-field
-            v-model="visiblePage.description"
-            label="Descrizione"
-          />
-          <v-text-field
-            v-model="visiblePage.type"
-            label="Tipo"
-          />
+          <v-text-field v-model="visiblePage.title" label="Titolo" />
+          <v-text-field v-model="visiblePage.description" label="Descrizione" />
+          <v-text-field v-model="visiblePage.type" label="Tipo" />
           <v-combobox
             v-model="visiblePage.categories"
             :items="categories"
@@ -35,19 +23,11 @@
             label="Categoria"
             prepend-icon="category"
           />
-          <v-text-field
-            v-model="visiblePage.price"
-            label="Prezzo"
-          />
+          <v-text-field v-model="visiblePage.price" label="Prezzo" />
         </v-form>
       </v-flex>
       <v-card-actions>
-        <v-btn
-          color="grey darken-4"
-          class="white--text"
-          block
-          @click="Update"
-        >
+        <v-btn color="grey darken-4" class="white--text" block @click="Update">
           Modifica i dati
         </v-btn>
       </v-card-actions>
@@ -57,27 +37,27 @@
 
 <script>
 export default {
-  props: ['visiblePage'],
+  props: ["visiblePage"],
   data() {
     return {
-      dialog: false,
-    }
+      dialog: false
+    };
   },
   computed: {
     categories: {
       get() {
         return this.$store.getters["nftService/categories"];
+      }
     }
-    },
   },
- methods: {
+  methods: {
     Update() {
-      this.$store.dispatch("nftService/updateOpera");
-      this.dialog=false;
+      this.$store.dispatch("nftService/updateOpera", this.visiblePage);
+      this.dialog = false;
     }
   },
   created() {
     this.$store.dispatch("nftService/getCategories");
-  },
-}
+  }
+};
 </script>
