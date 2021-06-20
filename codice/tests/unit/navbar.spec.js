9d6b980c28b7a9e8272749a06ab141d40c7ebd53
Margherita <margherita.mitillo@gmail.com>
2021-06-20 20:23:17 +0200

    Add doc folder, organized folders

---
+++ codice/tests/unit/navbar.spec.js
@@ -0,0 +1,33 @@
+import Vuex from 'vuex';
+import Vuetify from 'vuetify';
+import { mount, createLocalVue, shallowMount } from '@vue/test-utils';
+import VueRouter from 'vue-router';
+import store from '@/store';
+import navBar from '@/components/Navbar.vue';
+
+const localVue = createLocalVue();
+
+describe('navBar.vue', () => {
+  let vuetify;
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+  it('Check if content render', () => {
+    const wrapper = shallowMount(navBar, {
+      localVue,
+      store,
+      vuetify
+    });
+    expect(wrapper.contains('nav')).toBe(true);
+  });
+  it('Should render user name if isLogged=true', () => {
+    const wrapper = shallowMount(navBar, {
+      localVue,
+      store,
+      vuetify
+    });
+    expect(wrapper.find('p').exists()).toBe(false);
+    wrapper.setData({ isLogged: true });
+    expect(wrapper.find('p').exists()).toBe(true);
+  });
+});
